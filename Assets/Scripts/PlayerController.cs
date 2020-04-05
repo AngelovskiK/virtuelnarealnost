ca0988eb253a450e083ad73113d20f515d0fc0f6
AngelovskiK <angelovskik6@gmail.com>
2020-04-05 19:53:05 +0200

    laboratoriska 1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody myRigidbody;
+    public float moveSpeed = 10;
+    public Transform cameraTransform;
+    public GvrReticlePointer reticlePointer;
+    private bool moving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {        
+        moving = Input.GetButton("Fire1") && !Input.GetKey(KeyCode.LeftAlt) && !Input.GetKey(KeyCode.LeftControl) && !reticlePointer.isOnInteractive;
+    }
+
+    void FixedUpdate()
+    {
+        if (moving)
+        {
+            myRigidbody.velocity = cameraTransform.forward * Time.deltaTime * moveSpeed;
+        } 
+    }
+}
---
+++ Assets/Scripts/RotateCube.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    private double rotateSpeed = 25;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotateSpeed -= 20 *  Time.deltaTime;
+        if (rotateSpeed < 0)
+            rotateSpeed = 0;
+        transform.Rotate(0, (float) (rotateSpeed * Time.deltaTime), 0);
+    }
+
+    public void IncreaseSpeed()
+    {
+        rotateSpeed += 35;
+        if(rotateSpeed > 100)
+        {
+            rotateSpeed = 100;
+        }
+    }
+}
